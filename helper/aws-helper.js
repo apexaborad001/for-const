6f746440fb2dab053657ff77b5fddb92d8c913f2
surendra <surendra@edooni.com>
2021-03-21 23:54:10 +0530

    add new column in uers table

--- helper/aws-helper.js
+++ helper/aws-helper.js
@@ -1,8 +1,5 @@
 var AWS = require('aws-sdk');
-const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
-});
+const s3 = new AWS.S3();
 var uuid = require('uuid');
 const fs = require("fs");
 const path = require("path");
