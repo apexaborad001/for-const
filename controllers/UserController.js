0242ba695ff12e8a8fd513ada27713f77bb240db
surendra-mobi <surendramaurya@mobikasa.com>
2021-03-30 22:17:22 +0530

    image upload test

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -18,6 +18,8 @@ const editProfile = async(req,res) =>{
             if(req.files){
                 try{
 					let imageRes = await helper.upload(req, "myprofile");
+          return res.status(200).json({ "imf":imageRes });
+
 					imageRes["userId"] = req.decoded.user_id;
 					await req.models.user_images.destroy({
                     	where:{
