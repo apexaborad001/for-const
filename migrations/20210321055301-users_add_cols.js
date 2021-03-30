2662fa3e654b7bcc3cb6c8a003f689126ebebd30
surendra-mobi <surendramaurya@mobikasa.com>
2021-03-30 17:25:08 +0530

    update in registration flow

--- migrations/20210321055301-users_add_cols.js
+++ migrations/20210321055301-users_add_cols.js
@@ -1,34 +1,12 @@
 'use strict';
 
 module.exports = {
-  up: async(queryInterface, Sequelize) => {
-    /*
-      Add altering commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
-    */
-    await queryInterface.addColumn('users', 'reset_password_token', {
-      	type: Sequelize.STRING,
-        allowNull: true
-    });
-    await queryInterface.addColumn('users', 'reset_password_expires', {
-      	type: Sequelize.STRING,
-        allowNull: true
-    });
+  up: async (queryInterface, Sequelize) => {
+  
     
   },
 
-  down: async(queryInterface, Sequelize) => {
-    /*
-      Add reverting commands here.
-      Return a promise to correctly handle asynchronicity.
-
-      Example:
-      return queryInterface.dropTable('users');
-    */
-    await queryInterface.removeColumn('users', 'reset_password_token');
-    await queryInterface.removeColumn('users', 'reset_password_expires');
+  down: async (queryInterface, Sequelize) => {
+   
   }
 };
