c12f8d0649dc17bb672d1292387c319c68a81c13
amitagrawal <amitagrawal@mobikasa.com>
2021-04-21 13:26:17 +0530

    reset game and structure

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require('dotenv').config();
 process.env.fromId = `NCRRUGBY <${process.env.mailFrom}>`;
 process.env.BUCKET_NAME = "ncrrugbyuat";
 const compression = require('compression');
-const path = require('path');
 var fileUpload = require('express-fileupload');
 const bodyParser = require('body-parser');
 const api_routes = require("./routes/api");
@@ -15,23 +14,19 @@ const resetDatabaseRoutes = require("./routes/resetDatabase");
 
 const models = require("./models");
 const dbSwitch = require("./middlewares/databaseSwitch");
-const checkAppVersion = require("./middlewares/checkAppVersion");
-const conRelease = require("./middlewares/releaseConnection");
 let logger = require("./helper/logger-helper")
 var mung = require('express-mung');
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./docs/swagger.json');
-const cron=require('./cron.js')
 
 const cors = require('cors')
-//const cron = require("./cron");
 const app = express();
 app.use(cors());
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, device_token, access_token,device_type, language");
   res.header("Access-Control-Expose-Headers", "Origin, X-Requested-With, Content-Type, Accept, device_token, access_token,device_type, language");
-res.header("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'  *")
+  res.header("Content-Security-Policy", "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'  *")
   next();
 });
 app.use(bodyParser.json({ limit: '50mb' }));
@@ -39,12 +34,9 @@ app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use((req, res, next) => {
   let hostname = req.hostname;
   let isSubdoamin = false;
-  //isSubdoamin = hostname.split(".").length > 2 ? true : false;
-  //req.isSubDomain = isSubdoamin;
   req.subDomain = hostname.split(".")[0];
   next();
 });
-// 
 
 app.use(compression());
 app.use(fileUpload());
@@ -52,9 +44,9 @@ const baseUrl = '/api/v1';
 app.use('/logs', express.static("logs"));
 app.use(`${baseUrl}/docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use('/assets', express.static("public"));
-app.engine('html', require('ejs').renderFile);  
+app.engine('html', require('ejs').renderFile);
 
-app.get('/pdfdemo', function(req, res) {
+app.get('/pdfdemo', function (req, res) {
   res.render(__dirname + '/test.html', {
     string: 'random_value',
     other: 'value'
@@ -81,29 +73,29 @@ app.use(`${baseUrl}/notifications`, dbSwitch, webpushNotificationsRoutes);
 app.use(`${baseUrl}/manage-user-bracket`, dbSwitch, userBracketRoutes);
 app.use(`${baseUrl}/leaderboard`, dbSwitch, leaderboardRoutes);
 
-app.get('/hello_world', (req,res)=>{
+app.get('/hello_world', (req, res) => {
   res.send('Hello World');
 })
 
-app.get('/hello_world2', (req,res)=>{
+app.get('/hello_world2', (req, res) => {
   res.send('Hello data 2');
 })
 
-app.get('/hello_world3', (req,res)=>{
+app.get('/hello_world3', (req, res) => {
   res.send('Hello data 3');
 })
 
 //end middlewares
 //connection checking
 models.sequelize.authenticate()
-  .then(async() => {
+  .then(async () => {
     try {
       let con = models.sequelize;
       await con.query(`SET sql_mode = ""`);
       //await con.close();
       let port = process.env.SERVER_PORT ? process.env.SERVER_PORT : 3000;
       app.listen(port, () => {
-        console.log(`started1 on port  ${port}`); 
+        console.log(`started1 on port  ${port}`);
         //cron.updateGames('*/10 * * * * * ');
       });
     } catch (err) {
