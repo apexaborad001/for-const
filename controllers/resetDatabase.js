004f254ea80b8690c8e12340e35da16262e54d2f
surendra-mobi <surendramaurya@mobikasa.com>
2021-05-04 16:12:32 +0530

    Reset All Data API

--- controllers/resetDatabase.js
+++ controllers/resetDatabase.js
@@ -49,8 +49,44 @@ const resetTournamentGames = async (req, res) => {
       })
     }
   }
+  const userReset = async (req, res) => {
+    try {
+      let sql1 = `SET FOREIGN_KEY_CHECKS=0;`;
+      let getQueryResult1 = await req.database.query(sql1, { type: req.database.QueryTypes.DELETE });
+      
+      let sql2 = `delete from users where admin = 0`;
+      let getQueryResult2 = await req.database.query(sql2, { type: req.database.QueryTypes.DELETE })
+      
+      let sql3 = `delete from devices;`;
+      let getQueryResult3 = await req.database.query(sql3, { type: req.database.QueryTypes.DELETE })
+      
+      
+       let sql4 = `delete from user_images;`;
+       let getQueryResult4 = await req.database.query(sql4, { type: req.database.QueryTypes.DELETE })
+      
+       let sql5 = `delete from user_breakets;`;
+       let getQueryResult5 = await req.database.query(sql5, { type: req.database.QueryTypes.DELETE })
+       
+       let sql = `delete from user_breaket_teams;`;
+       let getQueryResult6 = await req.database.query(sql, { type: req.database.QueryTypes.DELETE })
+      res.status(req.constants.HTTP_SUCCESS).json({
+        code: req.constants.HTTP_SUCCESS,
+        status: req.constants.SUCCESS,
+        message: "deleted",
+        data:{getQueryResult1, getQueryResult2, getQueryResult3, getQueryResult4, getQueryResult5}
+      })
+    }
+    catch (err) {
+      res.status(req.constants.HTTP_SERVER_ERROR).json({
+        status: req.constants.ERROR,
+        code: req.constants.HTTP_SERVER_ERROR,
+        message: req.messages.INTERNAL500 + err
+      })
+    }
+  }
   
   module.exports = {
     resetTournamentGames,
-    userBracketReset
+    userBracketReset,
+    userReset
   }
--- routes/resetDatabase.js
+++ routes/resetDatabase.js
@@ -7,5 +7,6 @@ const routes = require('express').Router();
 
 routes.get('/resetTournamentGames', resetDatabaseController.resetTournamentGames);
 routes.get("/userBracketReset/:email",resetDatabaseController.userBracketReset);
+routes.get("/resetAllUsers", resetDatabaseController.userReset);
 
 module.exports = routes;
