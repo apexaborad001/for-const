06c19c0bdfc22c11057f393e7d69d10a9f60c9a3
surendra-mobi <surendramaurya@mobikasa.com>
2021-05-11 17:54:42 +0530

    update in push notification

--- routes/webpushNotifications.js
+++ routes/webpushNotifications.js
@@ -6,7 +6,7 @@ const auth = require("../middlewares/auth");
 const routes = require('express').Router();
 
 routes.post("/unsubscribe",auth.isAuthenticated,webpushNotificationController.unSubscribe);
-routes.post("/subscribe", webpushNotificationController.subscribe);
+routes.post("/subscribe", auth.isAuthenticated, webpushNotificationController.subscribe);
 routes.post("/sendNotification",auth.isAuthenticated, webpushNotificationController.sendNotification); 
 
 module.exports = routes;
